eaa065f49db034ae5bd1483e88b0db74eb7dbdb7
FernandoKain <fernandomanga@hotmail.com>
2022-03-25 11:45:22 -0300

    Programa terminado - Abstração de um Bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+import com.sun.security.jgss.GSSUtil;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,10 +21,39 @@ public class Main {
         mentoria.setDescricao("Descrição da mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devFernando = new Dev();
+        devFernando.setNome("Fernando");
+        devFernando.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devFernando.getConteudosInscritos());
+        devFernando.progredir();
+        System.out.println("Depois da progressão os cursos concluídos são retirados dos cursos inscritos:");
+        System.out.println("Conteúdos Inscritos" + devFernando.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devFernando.getConteudosConcluidos());
+        System.out.println("XP: " + devFernando.calcularTotalXp());
+        System.out.println("-------------------------------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("Depois da progressão os cursos concluídos são retirados dos cursos inscritos:");
+        System.out.println("Conteúdos Inscritos" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
 
     }
 }
